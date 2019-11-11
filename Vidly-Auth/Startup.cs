da3a0291929a833e7f78372c22a89e3a346719d2
pgeetha <gpriyapillai@gmail.com>
2019-11-11 12:08:26 +0100

    Before Database Connections

---
+++ Vidly-Auth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Auth.Startup))]
+namespace Vidly_Auth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
